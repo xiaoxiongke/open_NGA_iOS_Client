0b7686970323ab7b8aaf6d0ea6d0333681672e31
eorin <eorin@macbook.local>
2015-10-27 21:16:04 +0800

    Initial Commit

---
+++ NGA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NGA
+//
+//  Created by eorin on 15/10/27.
+//  Copyright © 2015年 eorin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
